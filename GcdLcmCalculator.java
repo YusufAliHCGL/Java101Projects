67a806cf42b610cc3e6f474a446d498a19668a02
Yusuf Ali Hacıoğlu <166921329+YusufAliHCGL@users.noreply.github.com>
2026-07-05 00:57:48 +0300

    Create GcdLcmCalculator.java

---
+++ GcdLcmCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class GcdLcmCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2;
+        int gcd,lcm;
+        System.out.print("Sayı1: ");
+        num1 = scanner.nextInt();
+        System.out.print("Sayı2: ");
+        num2 = scanner.nextInt();
+        if (num1 > 0 && num2 > 0) {
+            gcd = gcd(num1, num2);
+            lcm = lcm(num1, num2);
+            System.out.println(num1 + " ve " + num2 + " sayısının EBOB değeri: " + gcd);
+            System.out.println(num1 + " ve " + num2 + " sayısının EKOK değeri: " + lcm);
+        }
+    }
+    public static int gcd(int num1, int num2) {
+        int mNum;
+        int divisor = 1;
+        if (num1 < num2)
+            mNum = num1;
+        else
+            mNum = num2;
+        for (int i = 1; i <= mNum; i++) {
+            if (num1%i == 0 && num2%i == 0)
+                divisor = i;
+        }
+        return divisor;
+    }
+
+    public static int lcm(int num1, int num2) {
+        int mNum;
+        int multiple = 1;
+        if (num1 > num2)
+            mNum = num1;
+        else
+            mNum = num2;
+        for (int i = mNum; i <= (num1*num2); i++) {
+            if (i%num1 == 0 && i%num2 == 0) {
+                multiple = i;
+                break;
+            }
+        }
+        return multiple;
+    }
+}
